7186178f83e893d2f28abda318397b7ceff34573
zenzhu <zic.zhu@gmail.com>
2017-05-19 11:44:28 +0800

    Using less outside

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,7 @@
 
 <script>
 import Hello from './components/Hello'
+import './styles/index.less'
 
 export default {
   name: 'app',
@@ -15,14 +16,3 @@ export default {
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
